b9beb94f0787b0a2cf18e867af291ac43c720be5
ATofighi <alirtofighim@gmail.com>
2013-11-09 15:26:13 +0330

    fix "mybbirads.php" security issues

--- mybbirads.php
+++ mybbirads.php
@@ -1,26 +1,25 @@
 <?php
-
 define("IN_MYBB", 1);
 define("NO_ONLINE", 1);
 define('THIS_SCRIPT', 'mybbirads.php');
-
 require_once "./global.php";
-global $db;
-if($mybb->input['id'])
-{
-$query = $db->query("
-	SELECT *
-	FROM ".TABLE_PREFIX."mybbirads
-	WHERE id = '".$mybb->input['id']."'
-	LIMIT 1
-");
-$ads = $db->fetch_array($query);
 
-	header('Location:'.$ads['url']);
-	
-$db->write_query("
-		UPDATE ".TABLE_PREFIX."mybbirads
-		SET click = click+1
-	    WHERE id = ".$mybb->input['id']." LIMIT 1");
-}	
+if(isset($mybb->input['id']) && intval($mybb->input['id']) > 0)
+{
+	$query = $db->query("
+		SELECT *
+		FROM ".TABLE_PREFIX."mybbirads
+		WHERE id = '".intval($mybb->input['id'])."' and enabled = '1'
+		LIMIT 1
+	");
+	if($db->num_rows($query) == 1) {
+		$ads = $db->fetch_array($query);
+		header('Location:'.$ads['url']);
+		$db->write_query("
+				UPDATE ".TABLE_PREFIX."mybbirads
+				SET click = click+1
+				WHERE id = '".intval($mybb->input['id'])."' and enabled = '1' LIMIT 1");
+	}
+}
+exit;
 ?>
