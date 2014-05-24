1c6d6871b3d1d607d50d3606a23a2c1490b661c3
AliReza_Tofighi <alirtofighim@gmail.com>
2014-05-24 17:38:14 +0430

    حل مشکل #7

--- mybbirads.php
+++ mybbirads.php
@@ -5,25 +5,32 @@ define("NO_ONLINE", 1);
 define('THIS_SCRIPT', 'mybbirads.php');
 
 require_once "./global.php";
-global $db;
+
+if(isset($mybb->input['id'])) {
+	$mybb->input['id'] = intval($mybb->input['id']);
+} else {
+	$mybb->input['id'] = 0;
+}
+
 if($mybb->input['id'])
 {
-$query = $db->query("
-	SELECT *
-	FROM ".TABLE_PREFIX."mybbirads
-	WHERE id = '".$mybb->input['id']."'
-	LIMIT 1
-");
-$ads = $db->fetch_array($query);
+	$query = $db->query("
+		SELECT *
+		FROM ".TABLE_PREFIX."mybbirads
+		WHERE id = '".$mybb->input['id']."'
+		LIMIT 1
+	");
+	$ads = $db->fetch_array($query);
 	if(substr($ads['url'], 0, 7) != 'http://' && substr($ads['url'], 0, 8) != 'https://' && substr($ads['url'], 0, 6) != 'ftp://')
 	{
 		$ads['url'] = 'http://'.$ads['url'];
 	}
 	header('Location: '.$ads['url']);
-	
-$db->write_query("
-		UPDATE ".TABLE_PREFIX."mybbirads
-		SET click = click+1
-	    WHERE id = ".$mybb->input['id']." LIMIT 1");
+		
+	$db->write_query("
+			 UPDATE ".TABLE_PREFIX."mybbirads
+			 SET click = click+1
+			 WHERE id = ".$mybb->input['id']."
+			 LIMIT 1");
 }	
 ?>
